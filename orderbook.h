9d38b0306afbc69574558b44be38edb69fae1e58
namaneee <naman4242sinha@gmail.com>
2026-07-09 08:50:05 +0530

    Format orderbook.h for consistency

--- orderbook.h
+++ orderbook.h
@@ -9,9 +9,9 @@ public:
     void printBook() const;
 
 private:
-    // Bids: highest price first (greater<double>)
+    
     std::map<double, std::queue<Order>, std::greater<double>> bids;
-    // Asks: lowest price first (default)
+    
     std::map<double, std::queue<Order>> asks;
 
     void match();
